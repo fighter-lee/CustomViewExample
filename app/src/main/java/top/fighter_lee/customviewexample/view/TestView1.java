80ad5472cef7f61c8f7edfbf8dbde68cf73e236a
fighter_lee <fighterlee1@gmail.com>
2017-11-15 20:06:25 +0800

    Initial commit

---
+++ app/src/main/java/top/fighter_lee/customviewexample/view/TestView1.java
@@ -0,0 +1,52 @@
+package top.fighter_lee.customviewexample.view;
+
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+import top.fighter_lee.customviewexample.R;
+
+/**
+ * @author fighter_lee
+ * @date 2017/11/15
+ */
+public class TestView1 extends View {
+    public TestView1(Context context) {
+        super(context);
+    }
+
+    public TestView1(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TestView1(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        float left = getLeft();
+        float right = getRight();
+        float top = getTop();
+        float bottom = getBottom();
+        RectF rectF = new RectF(left, top, right, bottom);
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+//        paint.setColor(Color.parseColor("#FF4081"));
+        paint.setColor(getResources().getColor(android.R.color.black));
+        //画方
+//        canvas.drawRect(left,top,right,bottom,paint);
+        //画圆
+//        canvas.drawCircle(getWidth()/2,getHeight()/2,Math.min(getWidth(),getHeight())/2,paint);
+        //画扇形
+//        canvas.drawArc(rectF,0,90,false,paint);
+        //绘制bitmap
+        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher),right/2,bottom/2,paint);
+    }
+}
