8683850ae2df26209ee062c0a6aca0548834c971
liyangbiao <fighter_lee1@gmail.com>
2017-11-16 00:31:09 +0800

    1

--- app/src/main/java/top/fighter_lee/customviewexample/view/TestView1.java
+++ app/src/main/java/top/fighter_lee/customviewexample/view/TestView1.java
@@ -1,15 +1,13 @@
 package top.fighter_lee.customviewexample.view;
 
 import android.content.Context;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.graphics.Typeface;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
-
-import top.fighter_lee.customviewexample.R;
 
 /**
  * @author fighter_lee
@@ -38,6 +36,9 @@ public class TestView1 extends View {
         float bottom = getBottom();
         RectF rectF = new RectF(left, top, right, bottom);
         Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setTypeface(Typeface.create(Typeface.SANS_SERIF,Typeface.BOLD));
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(4);
 //        paint.setColor(Color.parseColor("#FF4081"));
         paint.setColor(getResources().getColor(android.R.color.black));
         //画方
@@ -47,6 +48,22 @@ public class TestView1 extends View {
         //画扇形
 //        canvas.drawArc(rectF,0,90,false,paint);
         //绘制bitmap
-        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher),right/2,bottom/2,paint);
+//        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher),right/2,bottom/2,paint);
+        //写文字
+        for (int i = 0; i < 10; i++) {
+            String text = "hello";
+            paint.setTextSize(100f);
+            float measureText = paint.measureText(text);
+            canvas.drawText(text,right/2-measureText/2,bottom/2,paint);
+            canvas.translate(0,100);
+        }
+        invalidate();
+        //划线
+//        Path p = new Path();
+//        p.moveTo(100, 100);
+//        p.lineTo(200, 50);
+//        p.lineTo(300, 100);
+//        p.lineTo(200,400);
+//        canvas.drawPath(p,paint);
     }
 }
